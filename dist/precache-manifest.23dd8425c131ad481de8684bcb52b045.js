2ff34a4c4eb0fb0f3b0edbbc2d0c28f4b8cb960a
wendy <wendy556609@gmail.com>
2019-06-17 22:41:27 +0800

    vue

---
+++ dist/precache-manifest.23dd8425c131ad481de8684bcb52b045.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "20279b70ef7b23c816bd1f0b203f8114",
+    "url": "/img/A3.20279b70.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6652411628192426b33b88b2eeece65e",
+    "url": "/img/A2.66524116.jpg"
+  },
+  {
+    "revision": "c780bfe7e515db6c0ae2",
+    "url": "/js/chunk-2d0aa661.10ff9ef6.js"
+  },
+  {
+    "revision": "eaf2a60110ee2ded7c62",
+    "url": "/js/chunk-2d21f2da.649174e3.js"
+  },
+  {
+    "revision": "b8d4ba391ff7be7daff8",
+    "url": "/js/chunk-2d21dc81.9247e2c0.js"
+  },
+  {
+    "revision": "d0296c1e8213533af1f9",
+    "url": "/js/chunk-vendors.76233d69.js"
+  },
+  {
+    "revision": "a3cef36e9c4418ab56e2",
+    "url": "/js/app.189e0c83.js"
+  },
+  {
+    "revision": "7a2aa7a12bf237150ebb98ae5e2735d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75306e6e08e02a90d7bc8ea0d4babca2",
+    "url": "/img/Tbg.75306e6e.jpg"
+  },
+  {
+    "revision": "4f7cbc0cfdee3ec02bd2489beffc7ef0",
+    "url": "/img/Tbg-b.4f7cbc0c.jpg"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "d0296c1e8213533af1f9",
+    "url": "/css/chunk-vendors.19b4ccc8.css"
+  },
+  {
+    "revision": "a3cef36e9c4418ab56e2",
+    "url": "/css/app.ee9b861f.css"
+  }
+];
